9b24e499da98c6e0f6ecf05741e885ab339c6ed5
Etima Udo <etimastik@gmail.com>
2022-09-08 21:05:08 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
-#!/bin/bash
-include <stdio.h>
+#include <stdio.h>
 
 /**
- * main - prints the phrase with proper grammar,
- * but the outcome is a piece of art,
- * Return: 0 if exited properly, non-zero otherwise
+ *main - prints a space
+ *Return: Always 0 (Sucess)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }

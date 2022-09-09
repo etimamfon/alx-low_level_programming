2d6b663921ae02fa667264b2181a5f3525a48f83
Etima Udo <etimastik@gmail.com>
2022-09-08 21:27:06 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
-
+ #include <stdio.h>
 /**
- *main - prints a space
- *Return: Always 0 (Sucess)
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

1a2059438088bad13ab32ec129b0f963ab310bdb
Etima Udo <etimastik@gmail.com>
2022-09-14 16:16:44 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,17 +8,18 @@ int print_sign(int n);
 {
 	if (n > 0)
 	{
-	_putchar('+');
+	_putchar(43);
 	return (1);
 	}
 	else if (n < 0)
 	{
-	_putchar('-')
+	_putchar(45)
 	return (-1);
 	}
 	else
 	{
-	_putchar('0');
+	_putchar(48);
 	return (0);
 	}
+	_putchar('\n');
 }

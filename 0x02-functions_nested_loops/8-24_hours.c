5fa9ca67162235f343425ca5ee023bab87a6a040
Etima Udo <etimastik@gmail.com>
2022-09-14 18:23:03 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,6 +2,7 @@
 /**
  * jack_bauer - func that prints every minute of the day of Jack Bauer, starti
  * n from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
+ * and resets mins
  * Return: 0
  */
 void jack_bauer(void)

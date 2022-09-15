1b15d82e72aa5218145fb452535784f8eb3cce7c
Etima Udo <etimastik@gmail.com>
2022-09-14 18:56:34 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,26 +2,26 @@
 /**
  * print_sign - function to check for a sign of a number
  * greater, equal or less than zero.
- *
- * @n: is the int that will be used for the arguement of the function
- *
- * Return: -1 is less than zero.
+ * @n: is the int that will use for the arguement of the function
+ * Return: 0
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-	_putchar(43);
+	_putchar('+');
 	return (1);
 	}
+
 	else if (n < 0)
 	{
-	_putchar(45)
+	_putchar('-')
 	return (-1);
 	}
+
 	else
 	{
-	_putchar(48);
+	_putchar('0');
 	return (0);
 	}
 	_putchar('\n');

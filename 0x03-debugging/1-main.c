0ef7d3f7139a8f6ac274730b4987bb1f42502ced
Etima Udo <etimastik@gmail.com>
2022-09-15 12:36:41 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,9 +8,7 @@
 int main(void)
 {
 	int i;
-
 	printf("infinite loop incoming :(\n");
-
 	i = 0;
 	/*
 	*while (i < 10)
@@ -19,7 +17,6 @@ int main(void)
 	*}
 	*/
 	printf("infinite loop avoided! \\o/\n");
-
 	return (0);
 }
 
